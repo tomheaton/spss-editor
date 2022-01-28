503cc8bfffa093d9e3c0c02731c0645f55879f35
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-28 22:07:11 +0000

    work

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # spss-editor
+
+Editor for SPSS files.
+
+> SPSS Statistics Data File Format Family (.sav), formerly known as SPSS System File Format.
+
+## How to install.
+
+```bash
+pip install pyreadstat pandas numpy
+```
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+import pandas as pd
+import numpy as np
+import pyreadstat
+
 
 def main():
-    pass
+    # data = pd.read_spss(os.path.join(os.path.dirname(__file__), './data/Profile1.sav'))
+    # data.tail()
+    df, meta = pyreadstat.read_sav(os.path.join(os.path.dirname(__file__), './data/Profile1.sav'))
 
 
 if __name__ == '__main__':
